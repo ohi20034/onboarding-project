484a5b3b075f9f51006d21da0c63f06e29dbe733
Imdadul Haque Ohi <imdadul0202@gmail.com>
2024-08-11 01:55:45 +0600

    desing admin panel

---
+++ pages/forgot/script_forgot.js
@@ -0,0 +1,40 @@
+const searchInput = document.getElementById("searchInput");
+const searchButton = document.getElementById("searchButton");
+const navLastPart = document.querySelector(".nav-last-part");
+const header_container = document.querySelector(".container");
+let isSearchExpanded = true;
+
+searchButton.addEventListener("click", function () {
+  const screenWidth = window.innerWidth;
+  if (screenWidth <= 450) {
+    if (isSearchExpanded) {
+      navLastPart.style.display = "none";
+      searchInput.style.display = "flex";
+      searchButton.style.borderRadius = "0 8px 8px 0";
+      searchInput.style.fontSize = "16px";
+    } else {
+      navLastPart.style.display = "flex";
+      searchInput.style.display = "none";
+      searchButton.style.borderRadius = "8px";
+    }
+    isSearchExpanded = !isSearchExpanded;
+  }
+});
+
+document.addEventListener("scroll", function () {
+  console.log(window.scrollY);
+  if (window.scrollY >= 30.0) {
+    header_container.style.backgroundColor = "#b0d1c8";
+  } else {
+    header_container.style.backgroundColor = "";
+  }
+});
+
+window.addEventListener('resize', function() {
+  var formDiv = document.querySelector('form > div');
+  if (window.innerWidth <= 768) {
+      formDiv.style.width = '100%';
+  } else {
+      formDiv.style.width = '50%';
+  }
+});
